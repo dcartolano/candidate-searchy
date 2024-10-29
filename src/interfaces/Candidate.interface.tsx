76971d7570330b846536b2b9ff03ec869cfb8347
dcartolano <180475468+dcartolano@users.noreply.github.com>
2024-10-28 19:13:51 -0500

    added inital code for working candidate interface

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,20 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+export default interface Candidate {
+    readonly name: string | null;
+    readonly login: string | null;
+    readonly location: string | null;
+    readonly avatar_url: string | null;
+    readonly email: string | null;
+    readonly html_url: string | null;
+    readonly company: string | null;
+    readonly bio: string | null;
+  }
+  
+// name -> name
+// username -> login
+// location -> location
+// avatar -> avatar_url
+// email -> email
+// html_url -> html_url
+// company ->company
+// bio -> bio
