fada8046ec853efa896242c79984b1d33ba3c7c1
dcartolano <180475468+dcartolano@users.noreply.github.com>
2024-11-01 17:24:17 -0500

    updated to reflect a complete typing of all parts of the candidate object

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,15 +1,40 @@
 // TODO: Create an interface for the Candidate objects returned by the API
 export default interface Candidate {
-    readonly name: string | null;
-    readonly login: string | null;
-    readonly location: string | null;
-    readonly avatar_url: string | null;
-    readonly email: string | null;
-    readonly html_url: string | null;
-    readonly company: string | null;
-    readonly bio: string | null;
-  }
-  
+  readonly avatar_url: string | null;
+  readonly bio: string | null;
+  readonly blog: string | null;
+  readonly company: string | null;
+  readonly created_at: string | null;
+  readonly email: string | null;
+  readonly events_url: string | null;
+  readonly followers: number | null;
+  readonly followers_url: string | null;
+  readonly following: number | null;
+  readonly following_url: string | null;
+  readonly gists_url: string | null;
+  readonly gravatar_id: string | null;
+  readonly hireable: string | null;
+  readonly html_url: string | null;
+  readonly id: number | null;
+  readonly login: string | null;
+  readonly location: string | null;
+  readonly name: string | null;
+  readonly node_id: string | null;
+  readonly organizations_url: string | null;
+  readonly public_gists: number | null;
+  readonly public_repos: number | null;
+  readonly received_events_url: string | null;
+  readonly repos_url: string | null;
+  readonly site_admin: boolean | null;
+  readonly starred_url: string | null;
+  readonly subscriptions_url: string | null;
+  readonly twitter_username: string | null;
+  readonly type: string | null;
+  readonly updated_at: string | null;
+  readonly url: string | null;
+  readonly user_view_type: string | null;
+}
+
 // name -> name
 // username -> login
 // location -> location
@@ -18,3 +43,4 @@ export default interface Candidate {
 // html_url -> html_url
 // company ->company
 // bio -> bio
+
