5b013b50ce03f91ce311cb5cb9f475edba1d39ef
dcartolano <180475468+dcartolano@users.noreply.github.com>
2024-11-04 21:27:59 -0600

    added in initial candidate type to try and fix error "cannot read properties of undefined (reading 'login')

---
+++ src/interfaces/InitialCandidate.interface.tsx
@@ -0,0 +1,24 @@
+// TODO: Create an interface for the Candidate objects returned by the API
+export default interface InitialCandidate {
+    readonly avatar_url: string | null;
+    readonly events_url: string | null;
+    readonly followers_url: string | null;
+    readonly following_url: string | null;
+    readonly gists_url: string | null;
+    readonly gravatar_id: string | null;
+    readonly html_url: string | null;
+    readonly id: number | null;
+    readonly login: string;
+    readonly node_id: string | null;
+    readonly organizations_url: string | null;
+    readonly received_events_url: string | null;
+    readonly repos_url: string | null;
+    readonly site_admin: boolean | null;
+    readonly starred_url: string | null;
+    readonly subscriptions_url: string | null;
+    readonly type: string | null;
+    readonly url: string | null;
+    readonly user_view_type: string | null;
+  }
+  
+  
