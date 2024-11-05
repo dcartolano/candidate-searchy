2618aa8f7daa043c72c99e5fa55294b1d9196d48
dcartolano <180475468+dcartolano@users.noreply.github.com>
2024-11-04 18:45:11 -0600

    commented out console logs

--- src/api/API.tsx
+++ src/api/API.tsx
@@ -1,8 +1,8 @@
 const searchGithub = async () => {
   try {
     const start = Math.floor(Math.random() * 100000000) + 1;
-    console.log(import.meta.env);
-    console.log(import.meta.env.VITE_GITHUB_TOKEN);
+    // console.log(import.meta.env);
+    // console.log(import.meta.env.VITE_GITHUB_TOKEN);
     const response = await fetch(`https://api.github.com/users?since=${start}`, {
         headers: {
           Authorization: `Bearer ${import.meta.env.VITE_GITHUB_TOKEN}`,
