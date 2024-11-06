aa08d59bd10e82aaa69908a0973f9dd4baf7900a
dcartolano <180475468+dcartolano@users.noreply.github.com>
2024-11-06 08:47:52 -0600

    changed link import to have single quotes instead of double, changed to camel case for props, added in case for if user's name is blank on their profile and to use thier username in its place if that's the case. also added commented out section at the end that's in the process of being adjusted to be used for a table instead of a list of candidate cards

--- src/components/CandidateRow.tsx
+++ src/components/CandidateRow.tsx
@@ -1,10 +1,10 @@
 // import type React from 'react';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 import type Candidate from '../interfaces/Candidate.interface';
 
 type CandidateRowProps = {
-    PotentialCandidate: Candidate;
-      RejectCandidate?: (() => void);
+    potentialCandidate: Candidate;
+    rejectCandidate?: (() => void);
     //     | ((
     //         e: React.MouseEvent<SVGSVGElement, MouseEvent>,
     //         currentlyOnWatchList: boolean | null | undefined,
@@ -14,42 +14,43 @@ type CandidateRowProps = {
     //     | null;
 };
 
-const CandidateRow = ({ PotentialCandidate
-     , RejectCandidate
+const CandidateRow = ({ potentialCandidate, rejectCandidate
 }: CandidateRowProps) => {
 
-console.log(PotentialCandidate);
+    console.log('Potential Candidate', potentialCandidate);
+
+    const realName: string | null = potentialCandidate.name === null ? potentialCandidate.login : potentialCandidate.name;
 
     return (
         <>
             <section className='candidateCard'>
                 <figure>
-                    <img src={`${PotentialCandidate.avatar_url}`} alt={`${PotentialCandidate.login}`} />
+                    <img src={`${potentialCandidate.avatar_url}`} alt={`${potentialCandidate.login}`} />
                 </figure>
                 <article className='details'>
-                    <h2>{`${PotentialCandidate.name} ${PotentialCandidate.login}`}</h2>
+                    <h2>{`${realName} (${potentialCandidate.login})`}</h2>
                     <p>
-                        <strong>Location:</strong> {PotentialCandidate.location}
+                        <strong>Location:</strong> {potentialCandidate.location}
                     </p>
                     {/* email using link tag */}
                     <Link
-                        to={`${PotentialCandidate.email}`}
+                        to={`${potentialCandidate.email}`}
                     >
-                        <strong>Email:</strong> {PotentialCandidate.email}
+                        <strong>Email:</strong> {potentialCandidate.email}
                     </Link>
                     {/* email using anchor tag */}
                     {/* <a href={`${currentCandidate.email}`}><strong>Email:</strong> {currentCandidate.email}</a> */}
                     <p>
-                        <strong>Company:</strong> {PotentialCandidate.company}
+                        <strong>Company:</strong> {potentialCandidate.company}
                     </p>
                 </article>
                 <article className='bio'>
                     <p>
-                        <strong>Bio:</strong> {PotentialCandidate.bio}
+                        <strong>Bio:</strong> {potentialCandidate.bio}
                     </p>
                 </article>
                 {/* <button> */}
-                <button onClick={RejectCandidate}>
+                <button onClick={rejectCandidate}>
                     -
                 </button>
             </section>
@@ -58,3 +59,31 @@ console.log(PotentialCandidate);
 };
 
 export default CandidateRow;
+
+{/* <table>
+  <caption>
+    Potential Candidates
+  </caption>
+  <thead>
+    <tr>
+      <th scope="col">Image</th>
+      <th scope="col">Name</th>
+      <th scope="col">Location</th>
+      <th scope="col">Email</th>
+      <th scope="col">Company</th>
+      <th scope="col">Bio</th>
+      <th scope="col">Reject</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row"><img src={`${potentialCandidate.avatar_url}`} alt={`${potentialCandidate.login}`} /></th>
+      <td>${potentialCandidate.login}</td>
+      <td>9</td>
+      <td>Ever fallen in love (with someone you shouldn't've)</td>
+      <td>1976</td>
+      <td>9</td>
+      <td>Ever fallen in love (with someone you shouldn't've)</td>
+    </tr>
+  </tbody>
+</table> */}
