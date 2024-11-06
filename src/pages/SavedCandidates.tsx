02f86d80ad2b62e8b79ae8aa7dd3a205eb8fa498
dcartolano <180475468+dcartolano@users.noreply.github.com>
2024-11-06 09:50:51 -0600

    cleaned up code a little bit more

--- src/pages/SavedCandidates.tsx
+++ src/pages/SavedCandidates.tsx
@@ -1,4 +1,3 @@
-// import type React from 'react';
 import { useEffect, useState } from 'react';
 import CandidateRow from '../components/CandidateRow';
 import Candidate from '../interfaces/Candidate.interface';
@@ -7,20 +6,18 @@ const SavedCandidates = () => {
   const [potentialCandidates, setPotentialCandidates] = useState<Candidate[]>([]);
 
   const rejectCandidate = (
-  //   currentlyOnWatchList: boolean | null | undefined,
-  candidateLogin: string | null
+    candidateLogin: string | null
   ) => {
-      let parsedPotentialCandidates: Candidate[] = [];
+    let parsedPotentialCandidates: Candidate[] = [];
     const storedPotentialCandidates = localStorage.getItem('savedCandidates');
     if (typeof storedPotentialCandidates === 'string') {
       parsedPotentialCandidates = JSON.parse(storedPotentialCandidates);
     }
     parsedPotentialCandidates = parsedPotentialCandidates.filter(
-        (candidate) => candidate.login !== candidateLogin
-      );
-      setPotentialCandidates(parsedPotentialCandidates);
-      localStorage.setItem('savedCandidates', JSON.stringify(parsedPotentialCandidates));
-    // } 
+      (candidate) => candidate.login !== candidateLogin
+    );
+    setPotentialCandidates(parsedPotentialCandidates);
+    localStorage.setItem('savedCandidates', JSON.stringify(parsedPotentialCandidates));
   };
 
   useEffect(() => {
@@ -30,30 +27,23 @@ const SavedCandidates = () => {
     // console.log('parsedPotentialCandidates: ', parsedPotentialCandidates);
     setPotentialCandidates(parsedPotentialCandidates);
     // console.log('potentialCandidates: ', potentialCandidates); 
-    
   }, []);
 
   return (
     <>
-      {/* <h1 className='pageHeader'>Potential Candidates</h1> */}
-      { console.log('potentialCandidates: ', potentialCandidates) }
+      {/* {console.log('potentialCandidates: ', potentialCandidates)} */}
+      <h2>Potential Candidates</h2> 
       {(!potentialCandidates?.length || potentialCandidates?.length === 0) ? (
-        <h1 style={{ margin: '16px 0' }}>Add candidates to your potential candidates list.</h1>
-      ) : 
-      //  <h2>Candidate should appear here</h2> 
-      potentialCandidates.map((potentialCandidate) => 
-        (
-          // { // curly brackets were the issue for some reason?
-        //  <h2>potentialCandidate</h2> 
-        <CandidateRow
-          key={potentialCandidate.id}
-          potentialCandidate={potentialCandidate}
-          rejectCandidate={() => rejectCandidate(potentialCandidate.login)}
-        />
-        // }
-      )
-    )
-      }
+        <h2>Add candidates to your potential candidates list.</h2>
+      ) : (
+        potentialCandidates.map((potentialCandidate) =>
+          <CandidateRow
+            key={potentialCandidate.id}
+            potentialCandidate={potentialCandidate}
+            rejectCandidate={() => rejectCandidate(potentialCandidate.login)}
+          />
+        )
+      )}
     </>
   );
 };
