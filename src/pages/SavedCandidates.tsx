2b43d0ee7060a10a6c7aa8f977ecb7d950e358e3
dcartolano <180475468+dcartolano@users.noreply.github.com>
2024-11-06 08:54:31 -0600

    adjusted rejectCandidate code to match with this functions variables and declarations, and also took out unneeded parts. took out previous joke console log that was there to check if the function ran. switched to parens instead of curly brackets in map method, since this works for some reason, changed to camel case for CandidateRow props, and changed rejectCandidate function to pass with params

--- src/pages/SavedCandidates.tsx
+++ src/pages/SavedCandidates.tsx
@@ -7,39 +7,20 @@ const SavedCandidates = () => {
   const [potentialCandidates, setPotentialCandidates] = useState<Candidate[]>([]);
 
   const rejectCandidate = (
-  //   e: React.MouseEvent<SVGSVGElement, MouseEvent>,
   //   currentlyOnWatchList: boolean | null | undefined,
-  //   currentlyOnSeenItList: boolean | null | undefined,
-  //   title: string | null
+  candidateLogin: string | null
   ) => {
-    console.log('nice job rejecting da candidate !! that guy REALLY sucks!!!')
-  //   e.preventDefault();
-  //   if (currentlyOnWatchList) {
-  //     let parsedFilmsToWatch: Film[] = [];
-
-  //     const storedFilmsToWatch = localStorage.getItem('filmsToWatch');
-  //     if (typeof storedFilmsToWatch === 'string') {
-  //       parsedFilmsToWatch = JSON.parse(storedFilmsToWatch);
-  //     }
-  //     parsedFilmsToWatch = parsedFilmsToWatch.filter(
-  //       (film) => film.Title !== title
-  //     );
-  //     setFilmsToWatch(parsedFilmsToWatch);
-  //     localStorage.setItem('filmsToWatch', JSON.stringify(parsedFilmsToWatch));
-  //   } else if (currentlyOnSeenItList) {
-  //     let parsedAlreadySeenFilms: Film[] = [];
-  //     const storedAlreadySeenFilms = localStorage.getItem('alreadySeenFilms');
-  //     if (typeof storedAlreadySeenFilms === 'string') {
-  //       parsedAlreadySeenFilms = JSON.parse(storedAlreadySeenFilms);
-  //     }
-  //     parsedAlreadySeenFilms = parsedAlreadySeenFilms.filter(
-  //       (film) => film.Title !== title
-  //     );
-  //     localStorage.setItem(
-  //       'alreadySeenFilms',
-  //       JSON.stringify(parsedAlreadySeenFilms)
-  //     );
-  //   }
+      let parsedPotentialCandidates: Candidate[] = [];
+    const storedPotentialCandidates = localStorage.getItem('savedCandidates');
+    if (typeof storedPotentialCandidates === 'string') {
+      parsedPotentialCandidates = JSON.parse(storedPotentialCandidates);
+    }
+    parsedPotentialCandidates = parsedPotentialCandidates.filter(
+        (candidate) => candidate.login !== candidateLogin
+      );
+      setPotentialCandidates(parsedPotentialCandidates);
+      localStorage.setItem('savedCandidates', JSON.stringify(parsedPotentialCandidates));
+    // } 
   };
 
   useEffect(() => {
@@ -49,22 +30,29 @@ const SavedCandidates = () => {
     // console.log('parsedPotentialCandidates: ', parsedPotentialCandidates);
     setPotentialCandidates(parsedPotentialCandidates);
     // console.log('potentialCandidates: ', potentialCandidates); 
+    
   }, []);
 
   return (
     <>
       {/* <h1 className='pageHeader'>Potential Candidates</h1> */}
+      { console.log('potentialCandidates: ', potentialCandidates) }
       {(!potentialCandidates?.length || potentialCandidates?.length === 0) ? (
         <h1 style={{ margin: '16px 0' }}>Add candidates to your potential candidates list.</h1>
-      ) : potentialCandidates.map((potentialCandidate) => {
-        // console.log('potentialCandidates: ', potentialCandidates);
-        // console.log('potentialCandidate: ', potentialCandidate);
+      ) : 
+      //  <h2>Candidate should appear here</h2> 
+      potentialCandidates.map((potentialCandidate) => 
+        (
+          // { // curly brackets were the issue for some reason?
+        //  <h2>potentialCandidate</h2> 
         <CandidateRow
           key={potentialCandidate.id}
-          PotentialCandidate={potentialCandidate}
-          RejectCandidate={rejectCandidate}
+          potentialCandidate={potentialCandidate}
+          rejectCandidate={() => rejectCandidate(potentialCandidate.login)}
         />
-      })
+        // }
+      )
+    )
       }
     </>
   );
